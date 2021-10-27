885ce42b972f0ec18658b0231da06613fbe3a102
alex <alexandre.cisse.ac@gmail.com>
2021-10-27 19:22:51 +0200

    feat: :sparkles: Added blank question to the game

--- App.js
+++ App.js
@@ -1,8 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import { ActivityIndicator, Alert, Keyboard, View, TouchableWithoutFeedback } from 'react-native';
+import {
+  ActivityIndicator,
+  Alert,
+  Keyboard,
+  View,
+  TouchableWithoutFeedback,
+} from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import ImageMultipleChoiceQuestion from './src/components/ImageMultipleChoiceQuestion';
 import OpenEndedQuestion from './src/components/OpenEndedQuestion';
+import FillInTheBlank from './src/components/FillInTheBlank';
 import Header from './src/components/Header';
 
 import styles from './App.styles';
@@ -88,27 +95,36 @@ const App = () => {
 
   return (
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-    <View style={styles.root}>
-      <StatusBar style="dark" />
-      <Header
-        progress={currentQuestionIndex / questions.length}
-        lives={lives}
-      />
-      {currentQuestion.type === 'IMAGE_MULTIPLE_CHOICE' ? (
-        <ImageMultipleChoiceQuestion
-          question={currentQuestion}
-          onCorrect={onCorrect}
-          onWrong={onWrong}
+      <View style={styles.root}>
+        <StatusBar style='dark' />
+        <Header
+          progress={currentQuestionIndex / questions.length}
+          lives={lives}
         />
-      ) : null}
-      {currentQuestion.type === 'OPEN_ENDED' ? (
-        <OpenEndedQuestion
-          question={currentQuestion}
-          onCorrect={onCorrect}
-          onWrong={onWrong}
-        />
-      ) : null}
-    </View>
+
+        {currentQuestion.type === 'FILL_IN_THE_BLANK' && (
+          <FillInTheBlank
+            question={currentQuestion}
+            onCorrect={onCorrect}
+            onWrong={onWrong}
+          />
+        )}
+
+        {currentQuestion.type === 'IMAGE_MULTIPLE_CHOICE' ? (
+          <ImageMultipleChoiceQuestion
+            question={currentQuestion}
+            onCorrect={onCorrect}
+            onWrong={onWrong}
+          />
+        ) : null}
+        {currentQuestion.type === 'OPEN_ENDED' ? (
+          <OpenEndedQuestion
+            question={currentQuestion}
+            onCorrect={onCorrect}
+            onWrong={onWrong}
+          />
+        ) : null}
+      </View>
     </TouchableWithoutFeedback>
   );
 };
